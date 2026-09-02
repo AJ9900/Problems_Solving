4a01433c6fdb5239ab4812abd6686e89640576a7
Ajay Chauhan <cajay4824@gmail.com>
2026-09-02 22:07:51 +0530

    ✅ MAXSUM77 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.java
+++ codechef/medium/MAXSUM77/solution.java
@@ -4,9 +4,44 @@ import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+    static long solve(int N, int K, int[] A) {
+        int window = N - K;
 
-	}
+        long sum = 0;
+
+        for (int i = 0; i < window; i++) {
+            sum += A[i];
+        }
+
+        long maxSum = sum;
+        
+        for (int i = window; i < N; i++) {
+            sum += A[i];
+            sum -= A[i - window];
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            int[] A = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+
+            System.out.println(solve(N, K, A));
+        }
+    }
 }
