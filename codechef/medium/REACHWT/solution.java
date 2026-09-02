3322347ec24acab2ad84ca2fa0dc94bee8edf26f
Ajay Chauhan <cajay4824@gmail.com>
2026-09-02 21:20:14 +0530

    ✅ REACHWT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/REACHWT/solution.java
+++ codechef/medium/REACHWT/solution.java
@@ -7,6 +7,17 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+	      int N=0, N1,N2;
+	      if( T="1";
+	      }
+	      else if (T=2){
+	          int N2 =30;
+	      }
+	      else{
+	          N =N1+N2;
+	      }
 	}
 }
