f2eaa9c265e6c87811097014a85e4762c77e2814
Ajay Chauhan <cajay4824@gmail.com>
2026-09-07 23:57:37 +0530

    ✅ Distinct Subsequences II | Hard | Java | Runtime: 5 ms (54.61%) | Memory: 43.4 MB | 2026-09-07

---
+++ leetcode/hard/0940-distinct-subsequences-ii/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        int MOD = 1000000007;
+        int[] dp = new int[26];
+
+        for (char c : s.toCharArray()) {
+            int index = c - 'a';
+
+            int total = 1;
+
+            for (int i = 0; i < 26; i++) {
+                total = (total + dp[i]) % MOD;
+            }
+
+            dp[index] = total;
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            ans = (ans + dp[i]) % MOD;
+        }
+
+        return ans;
+    }
+}
