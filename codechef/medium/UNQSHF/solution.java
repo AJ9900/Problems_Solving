af3965def782a64b8dba17aeb1db09d494f7d93f
Ajay Chauhan <cajay4824@gmail.com>
2026-09-09 21:39:30 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -1,43 +1,32 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Codechef
 {
-    public static void main(String[] args) throws java.lang.Exception
-    {
-        Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in);
+	 int N = sc.nextInt();
 
-        int T = sc.nextInt();
-
-        // while(T-- > 0)
-        // {
-            int N = sc.nextInt();
-
-            String A = sc.next();
-            String B = sc.next();
-
-            int aC = 0, bC = 0;
-
-            for(int i = 0; i < N; i++)
-            {
-                if(A.charAt(i) == 'a')
-                {
-                    aC++;
-                }
-
-                if(B.charAt(i) == 'a')
-                {
-                    bC++;
-                }
-            }
-
-            if(aC == bC)
-            {
-                System.out.println("NO");
-            }
-            else
-            {
-                System.out.println("YES");
-            }
-        // }
-    }
+     String A = sc.next();
+     String B = sc.next();
+     
+     int aC = 0,bC =0;
+     for(int i=0;i<N;i++)
+     {
+         if(A.charAt(i)=='a'){
+             aC++;
+         }
+         if(B.charAt(i)=='a'){
+             bC++;
+         }
+     } 
+     if(aC == bC){
+         System.out.println("NO");
+     }else{
+         System.out.println("YES");
+     }
+     
+}
 }
