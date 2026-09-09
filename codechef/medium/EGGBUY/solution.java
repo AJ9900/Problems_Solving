f41d72d36cf00e85d4fad365788751e0ecd8b534
Ajay Chauhan <cajay4824@gmail.com>
2026-09-09 20:34:29 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/EGGBUY/solution.java
+++ codechef/medium/EGGBUY/solution.java
@@ -7,6 +7,18 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+	
+		Scanner sc= new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		int F = sc.nextInt();
+		
+		if(X*12 < Y*12+F){
+		    System.out.println(X*12);
+		}
+		else{
+		    System.out.println(Y*12+F);
+		}
 
 	}
 }
