37711127480ff8d63edf98a3e7a7d245eb46eff4
Ajay Chauhan <cajay4824@gmail.com>
2026-09-09 21:15:09 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -8,20 +8,19 @@ class Codechef
 	{
 	Scanner sc = new Scanner(System.in);
 	 int N = sc.nextInt();
-	String []A = String [N];
-	String []B = String [N];
-	
-	for(int i=0; i<N; i++){
-	    
-	    A[i] = sc.next();
-	    B[i] = sc.next();
-	}
-	
-	
-	for(int i=0; i<N; i++){
-	    
-	   
-	}
-	
-	}
+
+     String A = sc.next();
+     String B = sc.next();
+     
+     for(int i=0;i<N;i++)
+     {
+         
+     if(A.charAt(i) == B.charAt(i)){
+         System.out.println("NO");
+     }else{
+         System.out.println("YES");
+     }
+     } 
+     
+}
 }
