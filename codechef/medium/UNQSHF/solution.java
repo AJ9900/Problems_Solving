99d8728047b27abc4c08c08183680f2054e8d45c
Ajay Chauhan <cajay4824@gmail.com>
2026-09-09 20:54:09 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -7,9 +7,18 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	Scanner sc = new Scanner(System.in);
+	 int N = sc.nextInt();
+	String []A = new A[N];
+	String []B = new B[N];
 	
-	int A = sc.nextString();
-	int B = sc.nextString();
+	for(int i=0; i<N; i++){
+	    
+	    A[i] = sc.nextString();
+	}
+	for(int i=0; i<N; i++){
+	    
+	    B[i] = sc.nextString();
+	}
 	
 	}
 }
