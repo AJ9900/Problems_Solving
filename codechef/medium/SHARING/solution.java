a116deebf1f5393a20ac5d2fc7f1069ce6dfecd8
Ajay Chauhan <cajay4824@gmail.com>
2026-09-16 21:21:56 +0530

    ✅ SHARING | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/SHARING/solution.java
+++ codechef/medium/SHARING/solution.java
@@ -14,7 +14,7 @@ class Codechef
        if(A>B){
            if((A+B)%2==0){
               
-               System.out.println(((A+B)/2)/2);
+               System.out.println();
            } else{
                System.out.println("-1");
            }
