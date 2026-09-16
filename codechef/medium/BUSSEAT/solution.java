67eca8123943d36419fa817dd70ef858d8b955ee
Ajay Chauhan <cajay4824@gmail.com>
2026-09-16 21:57:14 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.java
+++ codechef/medium/BUSSEAT/solution.java
@@ -4,21 +4,10 @@ import java.io.*;
 
 class Codechef
 {
-    public static void main (String[] args) throws java.lang.Exception
-    {
-        Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
 
-        int T = sc.nextInt();
-
-        while (T-- > 0)
-        {
-            int N = sc.nextInt();
-            int K = sc.nextInt();
-
-            if (K > N)
-                System.out.println(K - N);
-            else
-                System.out.println(0);
-        }
-    }
+	}
 }
