4deafe8ec74e07e034ded38acec3234e2dfcdc88
Ajay Chauhan <cajay4824@gmail.com>
2026-09-23 21:58:58 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.java
+++ codechef/medium/PGNATK/solution.java
@@ -5,8 +5,16 @@ import java.io.*;
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+	{   Scanner sc = new Scanner(System.in);
+	    int T = sc.nextInt();
+       while(T-->0){
+           
+           int N = sc.nextInt();
+           int K = sc.nextInt();
+           
+           if()
+           
+       }
 
 	}
 }
