b33a7f5f0e7c03f3213cf2bb7897dd2203413a15
Ajay Chauhan <cajay4824@gmail.com>
2026-09-23 22:11:22 +0530

    ✅ FALLPR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/FALLPR/solution.java
+++ codechef/medium/FALLPR/solution.java
@@ -2,11 +2,3 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
-}
